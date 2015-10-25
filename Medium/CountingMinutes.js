8f97a918811df160b1ff086ce26b5eb29d6eb4d0
danpotty <danpotmail@yahoo.com>
2015-10-25 13:26:11 -0700

    added descriptions

--- Medium/CountingMinutes.js
+++ Medium/CountingMinutes.js
@@ -1,3 +1,5 @@
+// Using the JavaScript language, have the function CountingMinutes(str) take the str parameter being passed which will be two times (each properly formatted with a colon and am or pm) separated by a hyphen and return the total number of minutes between the two times. The time will be in a 12 hour clock format. For example: if str is 9:00am-10:00am then the output should be 60. If str is 1:00pm-11:00am the output should be 1320.
+
 function PMchecker(timeStr){
   if(timeStr.indexOf('p') !== -1 || timeStr.indexOf('P') !== -1){
     return true;
